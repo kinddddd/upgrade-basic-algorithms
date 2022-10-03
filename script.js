0b262c93903c6e07a89edfb218a0fa956d0dd0e2
Kind <alvaromoreno21197@gmail.com>
2022-10-03 16:22:33 +0200

    Iteration#2

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ let myFavoriteHero = 'Hulk';
 let x = 50;
 let h = 5;
 let y = 10;
+
+const character = {
+    name: 'Jack Sparrow',
+    age: 10
+}
+
+character.age = 25;
+
+let firstName = 'Jon';
+let lastName = 'Snow';
+let age = 24;
+
+console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.")
