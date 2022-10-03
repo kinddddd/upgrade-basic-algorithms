4d764e9e571d3a6e1c0e5b2c6c5790c186049eb6
Kind <alvaromoreno21197@gmail.com>
2022-10-03 16:29:47 +0200

    Iteration#2 GOOD ONE

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ let lastName = 'Snow';
 let age = 24;
 
 console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.")
+
+const toy1 = {
+    name: 'Russ myYear', 
+    price: 19
+}
+
+const toy2 = {
+    name: 'Rallo mcKing',
+    price: 29
+}
+
+console.log(toy1.price + toy2.price)
+
+let globlaBasePrice = 10000;
+
+const car1 = {
+    name: 'BMW m&m',
+    basePrice: 50000,
+    finalPrice: 60000
+}
+
+const car2 = {
+    name: 'Chevrloet Corbina',
+    basePrice: 70000,
+    finalPrice: 80000
+}
+
+globlaBasePrice = 25000;
+
+car1.finalPrice = car1.basePrice + globlaBasePrice;
+car2.finalPrice = car2.basePrice + globlaBasePrice;
